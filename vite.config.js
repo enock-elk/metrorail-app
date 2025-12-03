efcfe7ad0cf621a257d94b08937770358eacd9d3
Enock <enockelk@gmail.com>
2025-12-03 21:52:48 +0200

    Fix 404 and file structure

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    // We removed 'minify: terser' to use the default (esbuild)
-    // It is faster and doesn't require extra installations.
+    rollupOptions: {
+      output: {
+        // Force the Javascript to be named 'app.js' in the 'js' folder
+        entryFileNames: 'js/app.js',
+        chunkFileNames: 'js/[name].js',
+        // Force the CSS to be named 'style.css' in the 'css' folder
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'css/style.css';
+          return 'assets/[name][extname]';
+        }
+      }
+    }
   }
 });
