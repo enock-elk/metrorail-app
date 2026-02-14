56b17ae0c8f050dcade05bb6234f35c2618e33ca
Enock Leo Kazembe <145878529+enock-elk@users.noreply.github.com>
2026-02-14 10:49:44 +0200

    Bump CACHE_NAME to v5.00.04 with fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'metrorail-next-train-v5.00.02'; // BUMPED: Added ignoreSearch fix + Safety Check
+const CACHE_NAME = 'metrorail-next-train-v5.00.04'; // BUMPED: Added ignoreSearch fix + Safety Check
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
